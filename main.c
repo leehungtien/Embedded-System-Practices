71045a35b2971de62c05c0879508424d3ebaef22
leehungtien <84466310+leehungtien@users.noreply.github.com>
2021-10-14 00:46:53 +0800

    Used UART to display messages on Tera Term

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-  /******************************************************************************
+/******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
   * (c) EE2028 Teaching Team
@@ -12,10 +12,13 @@
 #include "../../Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01_magneto.h"
 #include "../../Drivers/BSP/B-L475E-IOT01/stm32l475e_iot01_psensor.h"
 #include "stdio.h"
+#include <string.h>
 
 extern void initialise_monitor_handles(void);	// for semi-hosting support (printf)
 
 void MX_GPIO_Init(void);
+static void UART1_Init(void);
+UART_HandleTypeDef huart1;
 
 int main(void)
 {
@@ -23,8 +26,8 @@ int main(void)
 
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
-
 	MX_GPIO_Init();
+	UART1_Init();
 
 	/* Peripheral initializations using BSP functions */
 	BSP_ACCELERO_Init();
@@ -42,6 +45,7 @@ int main(void)
 	{
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_14);
 		A = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+
 
 		if (A == 0) {
 
@@ -73,13 +77,20 @@ int main(void)
 				float pressure_data;
 				pressure_data = BSP_PSENSOR_ReadPressure();
 
-				printf("Accel X : %f; Accel Y : %f; Accel Z : %f; Magneto X: %f; Magneto Y: %f; Magneto Z: %f; Pressure: %f\n", accel_data[0], accel_data[1], accel_data[2], magneto_data[0], magneto_data[1], magneto_data[2], pressure_data);
+				// Message to be printed out
+				char message[128];
+
+				// Use sprintf to join all the variables into a string
+				sprintf(message, "Accel X : %f; Accel Y : %f; Accel Z : %f; Magneto X: %f; Magneto Y: %f; Magneto Z: %f; Pressure: %f\n", accel_data[0], accel_data[1], accel_data[2], magneto_data[0], magneto_data[1], magneto_data[2], pressure_data);
+				HAL_UART_Transmit(&huart1, (uint8_t*)message, strlen(message),0xFFFF);
 			}
 
 			else if (counter == 6) {
 				float temp_data;
 				temp_data = BSP_TSENSOR_ReadTemp();			// read temperature sensor
-				printf("Temperature : %f\n", temp_data);
+				char message_temperature[32];
+				sprintf(message_temperature, "Temperature : %f\n", temp_data);
+				HAL_UART_Transmit(&huart1, (uint8_t*)message_temperature, strlen(message_temperature),0xFFFF);
 
 				// Reset counter
 				counter = 0;
@@ -118,3 +129,32 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 }
 
+static void UART1_Init(void)
+{
+    /* Pin configuration for UART. BSP_COM_Init() can do this automatically */
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_6;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Configuring UART1 */
+    huart1.Instance = USART1;
+    huart1.Init.BaudRate = 115200;
+    huart1.Init.WordLength = UART_WORDLENGTH_8B;
+    huart1.Init.StopBits = UART_STOPBITS_1;
+    huart1.Init.Parity = UART_PARITY_NONE;
+    huart1.Init.Mode = UART_MODE_TX_RX;
+    huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+    huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+    huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    if (HAL_UART_Init(&huart1) != HAL_OK)
+    {
+      while(1);
+    }
+
+}
